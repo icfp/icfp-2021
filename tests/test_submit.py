5b7fd76a9c41871d09f625a60adfc5bda83dd0fc
Brett Spradling <brett@spradling.me>
2021-07-10 11:42:14 -0700

    make

--- tests/test_submit.py
+++ tests/test_submit.py
@@ -8,10 +8,9 @@ import requests_mock
 
 
 class TestSubmit(TestCase):
-
     @requests_mock.Mocker()
-    @patch('solver.submit.internal_run')
-    @patch('solver.submit.get_api_token')
+    @patch("solver.submit.internal_run")
+    @patch("solver.submit.get_api_token")
     def test_submit(self, requests_mock, token_mock, run_mock):
         problem_id = 1
         expected_identifier = "123"
@@ -21,21 +20,22 @@ class TestSubmit(TestCase):
         requests_mock.register_uri(
             "POST",
             f"https://poses.live/api/problems/{problem_id}/solutions",
-            [{'text': f'{{ "id": "{expected_identifier}" }}'}]
+            [{"text": f'{{ "id": "{expected_identifier}" }}'}],
         )
 
         response = submit(problem_id)
         self.assertEqual(response.id, expected_identifier)
 
     @requests_mock.Mocker()
-    @patch('solver.submit.internal_run')
-    @patch('solver.submit.get_api_token')
+    @patch("solver.submit.internal_run")
+    @patch("solver.submit.get_api_token")
     def test_submit_fails(self, requests_mock, token_mock, run_mock):
         problem_id = 2
         requests_mock.register_uri(
-            'POST',
+            "POST",
             f"https://poses.live/api/problems/{problem_id}/solutions",
-            [{'status_code': 500}])
+            [{"status_code": 500}],
+        )
         run_mock.return_value = Solution([Point(1, 2), Point(2, 1)])
         token_mock.return_value = "fake_token"
 
