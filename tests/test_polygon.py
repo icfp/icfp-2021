03587b4066e9b6513a2ea1b3dd6bb25483c598ae
teemo <teemobean@gmail.com>
2021-07-10 11:25:00 -0700

    Fix in-hole (#5)
    * Fix in-hole
    
    What a pain
    
    * Brett ...

--- tests/test_polygon.py
+++ tests/test_polygon.py
@@ -17,7 +17,10 @@ class TestPolygon(TestCase):
     def test_in_polygon(self, point: Point, expected: bool) -> None:
         problem: Problem = load_problem(1)
         actual: bool = in_polygon(point, problem.hole)
-        self.assertEqual(actual, expected)
+        if expected:
+            self.assertTrue(actual, f"Expected {point} to be in or on the hole")
+        else:
+            self.assertFalse(actual, f"Expected {point} not to be in or on the hole")
 
     @parameterized.expand(
         [
@@ -47,10 +50,24 @@ class TestPolygon(TestCase):
             param(Point(2, 3), True),
             param(Point(5, 2), False),
             param(Point(6, 4), False),
+            param(Point(5, 6), False),
         ]
     )
     def test_in_diamond(self, point: Point, expected: bool) -> None:
         hole: Hole = [Point(3, 2), Point(5, 4), Point(3, 6), Point(1, 4)]
+        actual: bool = in_polygon(point, hole)
+        if expected:
+            self.assertTrue(actual, f"Expected {point} to be in or on the hole")
+        else:
+            self.assertFalse(actual, f"Expected {point} not to be in or on the hole")
+
+    @parameterized.expand(
+        [
+            param(Point(6, 4), False),
+        ]
+    )
+    def test_in_diamond_two(self, point: Point, expected: bool) -> None:
+        hole: Hole = [Point(5, 4), Point(3, 6), Point(1, 5), Point(3, 2)]
         actual: bool = in_polygon(point, hole)
         if expected:
             self.assertTrue(actual, f"Expected {point} to be in or on the hole")
