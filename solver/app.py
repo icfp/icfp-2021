6ea37a4a947616f374703fd1729374a135f845be
Nathan Howell <nathan.d.howell@gmail.com>
2021-07-09 19:17:42 -0700

    Add some garbage Z3 stuff

--- solver/app.py
+++ solver/app.py
@@ -1,6 +1,8 @@
 from pathlib import Path
-from typing import List, NamedTuple
+from typing import Iterable, List, NamedTuple
 import click
+import math
+import z3
 from pydantic.dataclasses import dataclass
 
 
@@ -11,6 +13,7 @@ PROBLEMS_DIR = ROOT_DIR / "problems"
 class Point(NamedTuple):
     x: int
     y: int
+
 
 Hole = List[Point]
 VertexIndex = int
@@ -61,10 +64,48 @@ def dislikes(hole: Hole, pose: Figure):
     return sum(min(distance(v, h) for v in pose["vertices"]) for h in hole)
 
 
+def bits_for(xs: Iterable[int]) -> int:
+    return math.log2(max(xs) + 1)
+
+
 @click.command()
 @click.argument("problem_number")
 def run(problem_number: int):
-    print("Hello", problem_number, load_problem(problem_number))
+    p = load_problem(problem_number)
+
+    opt = z3.Optimize()
+    x_sort = z3.BitVecSort(bits_for(i.x for i in p.hole))
+    y_sort = z3.BitVecSort(bits_for(i.y for i in p.hole))
+
+    # translate vertices to z3
+    vertices = p.figure.vertices
+    xs = [z3.BitVec(f"x_{i.x}", x_sort) for i in vertices]
+    ys = [z3.BitVec(f"y_{i.y}", y_sort) for i in vertices]
+
+    vertex, mk_vertex, _ = z3.TupleSort("vertex", (x_sort, y_sort))
+    vertices = [mk_vertex(x, y) for x, y in zip(xs, ys)]
+
+    # add a distinct constraint on the vertex points
+    opt.add(z3.Distinct(*vertices))
+
+    edges = p.figure.edges
+    print(edges)
+
+    foo = z3.BitVec("foo", x_sort)
+    bar = z3.BitVec("bar", x_sort)
+    opt.add(foo * foo > 48)
+    opt.add(foo > 1)
+    opt.add(bar < 10)
+    opt.add(foo + 1 == bar)
+    b = opt.maximize(foo)
+    print(b)
+
+    res = opt.check()  # == sat
+    print(res)
+
+    model = opt.model()
+    print(model)
+    print(model[foo])
 
 
 if __name__ == "__main__":
