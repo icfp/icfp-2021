a9b9d32373ec0a4f72c94802ddc332f41e584f83
Nathan Howell <nathan.d.howell@gmail.com>
2021-07-09 20:36:36 -0700

    why why why

--- solver/app.py
+++ solver/app.py
@@ -63,7 +63,14 @@ def load_problem(problem_number: int) -> Problem:
 def distance(p1: Point, p2: Point):
     delta_x = p1.x - p2.x
     delta_y = p1.y - p2.y
-    return (delta_x * delta_x) + (delta_y * delta_y)
+    if isinstance(delta_x, z3.BitVecRef) and isinstance(delta_y, z3.BitVecRef):
+        foo = delta_x.size() + delta_y.size()
+        d_x = z3.ZeroExt(max(0, delta_y.size() - delta_x.size()) + foo, delta_x)
+        d_y = z3.ZeroExt(max(0, delta_x.size() - delta_y.size()) + foo, delta_y)
+        delta_x, delta_y = d_x, d_y
+    pow_x = delta_x * delta_x
+    pow_y = delta_y * delta_y
+    return pow_x + pow_y
 
 
 def dislikes(hole: Hole, pose: Figure):
@@ -80,8 +87,10 @@ def run(problem_number: int):
     p = load_problem(problem_number)
 
     opt = z3.Optimize()
+    # x_sort = z3.BitVecSort(bits_for(max(i.x, i.y) for i in p.hole) * 2)
     x_sort = z3.BitVecSort(bits_for(i.x for i in p.hole))
     y_sort = z3.BitVecSort(bits_for(i.y for i in p.hole))
+    y_sort = x_sort
 
     # translate vertices to z3
     vertices = p.figure.vertices
@@ -94,6 +103,20 @@ def run(problem_number: int):
     # add a distinct constraint on the vertex points
     opt.add(z3.Distinct(*vertices))
 
+    # calculate edge distances
+    initial_distances = [distance(p.figure.vertices[p1], p.figure.vertices[p2]) for p1, p2 in p.figure.edges]
+    actual_distances = [distance(Point(xs[p1], ys[p1]), Point(xs[p2], ys[p2])) for p1, p2 in p.figure.edges]
+    for i, a in zip(initial_distances, actual_distances):
+        print(i, a)
+        # this should work:
+        # opt.add(i == a)
+
+        # opt.add(i-1 <= a)
+        # opt.add(a <= i+1)
+        # opt.assert_and_track(i == a, f"foo{i}")
+
+    # print(distances)
+
     edges = p.figure.edges
     print(edges)
 
@@ -101,6 +124,10 @@ def run(problem_number: int):
     # print(b)
 
     res = opt.check()  # == sat
+    # if str(res) != "sat":
+    #     core = opt.unsat_core()
+    #     print(core["foo20"])
+    #     print(core)
     print(res)
 
     model = opt.model()
