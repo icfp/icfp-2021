46382da5554cbeb6efa22dc8e1f910063c527891
Nathan Howell <nathan.d.howell@gmail.com>
2021-07-09 17:22:52 -0700

    Run black on tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,21 +4,7 @@ from solver.app import distance, Point
 
 
 class TestApp(TestCase):
-
-    @parameterized.expand([
-        param(
-            (1, 1),
-            (2, 2),
-            2
-        ),
-        param(
-            (4, 5),
-            (5, 10),
-            26
-        )
-    ])
+    @parameterized.expand([param((1, 1), (2, 2), 2), param((4, 5), (5, 10), 26)])
     def test_distance(self, first: Point, second: Point, expected: int):
         actual = distance(first, second)
         self.assertEqual(actual, expected)
-
-
--- tests/test_polygon.py
+++ tests/test_polygon.py
@@ -5,32 +5,16 @@ from solver.polygon import in_polygon
 
 
 class TestPolygon(TestCase):
-
-    @parameterized.expand([
-        param(
-            (100, 100),
-            False
-        ),
-        param(
-            (30, 7),
-            True
-        ),
-        param(
-            (30, 5),
-            True
-        ),
-        param(
-            (70, 95),
-            True
-        ),
-        param(
-            (95, 95),
-            True
-        )
-    ])
+    @parameterized.expand(
+        [
+            param((100, 100), False),
+            param((30, 7), True),
+            param((30, 5), True),
+            param((70, 95), True),
+            param((95, 95), True),
+        ]
+    )
     def test_in_polygon(self, point: Point, expected: bool) -> None:
         problem: Problem = load_problem(1)
         actual: bool = in_polygon(point, problem["hole"])
         self.assertEqual(actual, expected)
-
-
