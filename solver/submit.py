39bcf3662f804c8c2fdacb8f1464a03f6a2f1099
Nathan Howell <nathan.d.howell@gmail.com>
2021-07-11 12:42:33 -0700

    Always minimize app submissions

--- solver/submit.py
+++ solver/submit.py
@@ -18,7 +18,7 @@ def get_api_token() -> str:
 
 
 def submit(problem_id: int) -> Identifier:
-    solution = _run(problem_id).solution
+    solution = _run(problem_id, minimize=True).solution
     print(f"Submitting solution {solution.vertices} for problem {problem_id}")
 
     res = requests.post(
